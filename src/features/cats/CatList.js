f881c4e0db3162b7d344f8d9584031d87e5623d3
Philip Levy <philipdlevy@gmail.com>
2022-08-30 17:36:49 -0600

    commit tree c42137211fbc503396ecdc52d96923deed85af66

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,25 @@
 // write your CatList component here
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+function CatList(catPics) {
+
+    const cats = useSelector((state) => state.entities)
+
+    console.log("cats", cats)
+
+    const catImage = cats.map((cat) => {
+        return <a href={cat.url} alt={cat}><img src={cat.url}></img></a>
+    })
+
+
+    return (
+        <div>
+            {catImage}
+        </div>
+    )
+}
+
+export default CatList;
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCats } from "./catsSlice";
+
+import CatList from "./CatList"
 
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
+
+
+  console.log(catPics);
   return (
-    <div>
+    <div className="App">
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      {/* missing component */}
+      <CatList catPics={catPics}/>
     </div>
   );
 }
